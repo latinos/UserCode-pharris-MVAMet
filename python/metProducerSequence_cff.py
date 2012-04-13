c7cb6b301bd1f88e9fc0658b11686596561ac17c
pharris <>
2012-04-13 14:37:32 +0000

    small fixes

--- python/metProducerSequence_cff.py
+++ python/metProducerSequence_cff.py
@@ -1,16 +1,36 @@
 import FWCore.ParameterSet.Config as cms
-from CMGTools.External.JetIdParams_cfi import *
-from CMGTools.External.puJetIDAlgo_cff import PhilV1
+from CMGTools.External.puJetIDAlgo_cff import *
 
 mvaMet   = cms.EDProducer("MVAMetProducer",
-                          CorrJetName     = cms.InputTag("ak5PFJetsL2L3"),
+                          CorrJetName     = cms.InputTag("ak5PFJetsL1L2L3"),
                           JetName         = cms.InputTag("ak5PFJets"),
                           PFCandidateName = cms.InputTag("particleFlow"),
                           VertexName      = cms.InputTag("offlinePrimaryVertices"),
-                          JetPtMin        = cms.double(1.),
-                          dZMin           = cms.double(0.1),
-                          puJetIDAlgo     = PhilV1,
-                          JetIdParams     = JetIdParams
+                          JetPtMin        = cms.double(1.0),
+                          dZMin           = cms.double(0.2),
+                          impactParTkThreshold = cms.untracked.double(1.) ,
+                          tmvaWeights = cms.untracked.string("CMGTools/External/data/mva_JetID_v1.weights.xml"),
+                          tmvaMethod  = cms.untracked.string("JetID"),
+                          version = cms.untracked.int32(-1),
+                          tmvaVariables = cms.untracked.vstring(
+    "nvtx",
+    "jetPt",
+    "jetEta",
+    "jetPhi",
+    "dZ",
+    "d0",
+    "beta",
+    "betaStar",
+    "nCharged",
+    "nNeutrals",
+    "dRMean",
+    "frac01",
+    "frac02",
+    "frac03",
+    "frac04",
+    "frac05",
+    ),
+                          JetIdParams = JetIdParams
                           )
 
                           
--- test/testMetProducer.py
+++ test/testMetProducer.py
@@ -2,12 +2,11 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("analysis")
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1)
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(10)
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.load('JetMETCorrections.Configuration.JetCorrectionProducers_cff')
-process.load('CMG.JetIDAnalysis.jetanalyzer_cfi')
-process.load('CMG.MetAnalysis.metProducerSequence_cff')
+process.load('pharris.MVAMet.metProducerSequence_cff')
  
 #process.GlobalTag.globaltag = 'GR_R_42_V23::All'
 process.GlobalTag.globaltag = 'MC_44_V12::All'
@@ -26,6 +25,6 @@ process.output = cms.OutputModule("PoolOutputModule",
 )       
 
 #process.ana = cms.Sequence(.metSquence)
-process.ana      = cms.Sequence(process.ak5PFJetsL2L3*process.metSequence)
+process.ana      = cms.Sequence(process.ak5PFJetsL1L2L3*process.metSequence)
 process.p        = cms.Path(process.ana)
 process.outpath  = cms.EndPath(process.output)
